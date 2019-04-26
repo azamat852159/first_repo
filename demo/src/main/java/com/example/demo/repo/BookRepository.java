1e9864201ef66bfc748bd5fe8a005592885b4aa3
azik <azik@mail.ru>
2019-04-26 17:50:54 +0300

    deleted

--- demo/src/main/java/com/example/demo/repo/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.model.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book,Long> {
-}
--- demo/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO book(author , name) VALUES ('azamat','java');
-INSERT INTO book(author , name) VALUES ('pana','ci');
-INSERT INTO book(author , name) VALUES ('sania','pidr');
